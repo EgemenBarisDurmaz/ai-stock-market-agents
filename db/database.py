581b72666d09259ef555ed849869ab9aa813b77f
EgemenBarisDurmaz <egemenbdurmaz@gmail.com>
2026-04-19 21:30:16 +0200

    fix: SQLite path inside Docker container

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 
-DATABASE_URL = "sqlite:///./signals.db"
+DATABASE_URL = "sqlite:////app/data/signals.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
